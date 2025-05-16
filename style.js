9d68958edfa7bdfc6f2058943f9378f7a066dbbc
UDAY-exit <uu5071388@gmail.com>
2025-05-16 18:27:10 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,121 @@
+const questions = [
+  {
+    question: "which is largest animal in the world",
+    answers: [
+      { text: "shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "my name is :",
+    answers: [
+      { text: "udya", correct: false },
+      { text: "ram  lal", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "yop", correct: false },
+    ],
+  },
+  {
+    question: "when does Uday the Great born",
+    answers: [
+      { text: "1969", correct: false },
+      { text: "2000", correct: false },
+      { text: "2121", correct: false },
+      { text: "2005", correct: true },
+    ],
+  },
+  {
+    question: "Smallest country in the world",
+    answers: [
+      { text: "India", correct: false },
+      { text: "V-city", correct: true },
+      { text: "Russia", correct: false },
+      { text: "Paxtan", correct: false },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + " ." + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selctAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selctAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Play Agaian";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+startQuiz();
